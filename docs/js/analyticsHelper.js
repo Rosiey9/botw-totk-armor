11af560ed272758145519fa32c6adefeb243d7de
Rosiey9 <144098211+Rosiey9@users.noreply.github.com>
2023-09-05 02:32:34 -0600

    Create analyticsHelper.js

---
+++ docs/js/analyticsHelper.js
@@ -0,0 +1,53 @@
+function trackShare() {
+    ga('send', {
+        hitType: 'event',
+        eventCategory: 'button',
+        eventAction: 'click',
+        eventLabel: 'Share'
+    });
+}
+
+function trackRandom() {
+    ga('send', {
+        hitType: 'event',
+        eventCategory: 'button',
+        eventAction: 'click',
+        eventLabel: 'Random'
+    });
+}
+
+function trackShareTwitter() {
+    ga('send', {
+        hitType: 'event',
+        eventCategory: 'button',
+        eventAction: 'click',
+        eventLabel: 'ShareTwitter'
+    });
+}
+
+function trackShareFacebook() {
+    ga('send', {
+        hitType: 'event',
+        eventCategory: 'button',
+        eventAction: 'click',
+        eventLabel: 'ShareFacebook'
+    });
+}
+
+function trackDonate() {
+    ga('send', {
+        hitType: 'event',
+        eventCategory: 'button',
+        eventAction: 'click',
+        eventLabel: 'Donate'
+    });
+}
+
+function trackMyTwitter() {
+    ga('send', {
+        hitType: 'event',
+        eventCategory: 'button',
+        eventAction: 'click',
+        eventLabel: 'MyTwitter'
+    });
+}
